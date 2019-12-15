a85545ce044d72788789ef986dcdf8923c3327d5
hdpolover <hendrapolover@gmail.com>
2019-12-15 17:24:35 +0700

    updated

--- app/src/main/java/com/hdpolover/ybbadminproject/MainActivity.java
+++ app/src/main/java/com/hdpolover/ybbadminproject/MainActivity.java
@@ -4,10 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     TextView text;
 
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,31 @@ public class MainActivity extends AppCompatActivity {
 
         text = findViewById(R.id.text);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("a", "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            test();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("f", "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    private void test() {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Try");
         reference.child("1").setValue(true).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
@@ -37,5 +71,12 @@ public class MainActivity extends AppCompatActivity {
                 text.setText("No");
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+
     }
 }
