08e519f4615237d0891e4dfdd1fe95775854a541
hdpolover <hendrapolover@gmail.com>
2020-02-25 12:20:00 +0700

    updated

--- app/src/main/java/com/hdpolover/ybbadminproject/UsersTotalActivity.java
+++ app/src/main/java/com/hdpolover/ybbadminproject/UsersTotalActivity.java
@@ -1,12 +1,19 @@
 package com.hdpolover.ybbadminproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,12 +26,15 @@ import com.hdpolover.ybbadminproject.Models.ModelFeedback;
 import com.hdpolover.ybbadminproject.Models.ModelUsers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class UsersTotalActivity extends AppCompatActivity {
 
     TextView usersCountTv;
     RecyclerView recyclerView;
+    EditText searchUserEt;
+    Button searchBtn;
 
     AdapterUsers adapterUsers;
     List<ModelUsers> modelUsersList;
@@ -36,6 +46,8 @@ public class UsersTotalActivity extends AppCompatActivity {
 
         usersCountTv = findViewById(R.id.usersCountTv);
         recyclerView = findViewById(R.id.usersRecyclerView);
+        searchUserEt = findViewById(R.id.searchUserEt);
+        searchBtn = findViewById(R.id.searchBtn);
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -47,6 +59,15 @@ public class UsersTotalActivity extends AppCompatActivity {
 
         loadUsers();
         setUsersTotal(usersCountTv);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String searchQuery = searchUserEt.toString().trim();
+
+                searchUsers(searchQuery);
+            }
+        });
     }
 
     private void setUsersTotal(final TextView tv) {
@@ -81,6 +102,39 @@ public class UsersTotalActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
+    }
+
+    private void searchUsers(final String searchQuery) {
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                modelUsersList.clear();
+                for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                    ModelUsers modelUsers = ds.getValue(ModelUsers.class);
+
+                    if (modelUsers.getName().toLowerCase().contains(searchQuery.toLowerCase())
+                    || modelUsers.getEmail().toLowerCase().contains(searchQuery.toLowerCase())
+                    || modelUsers.getUsername().toLowerCase().contains(searchQuery.toLowerCase())) {
+                        modelUsersList.add(modelUsers);
+                    }
+
+                    //adapter
+                    adapterUsers = new AdapterUsers(getApplicationContext(), modelUsersList);
+                    //set adapter recycler view
+                    recyclerView.setAdapter(adapterUsers);
+                    adapterUsers.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                //in case of error
+                Toast.makeText(getApplicationContext(), ""+databaseError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
